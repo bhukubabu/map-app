2b45733c7bcfcf8d859ffbeac290824da0f46749
Bhukubabu <143258170+bhukubabu@users.noreply.github.com>
2024-11-11 23:44:13 +0530

    Update map_new.py

--- map_new.py
+++ map_new.py
@@ -8,18 +8,15 @@ from streamlit_folium import st_folium
 import streamlit.components.v1 as components
 from matplotlib import pyplot as plt
 
-st.title("Check safety status on the map 🗺")
-# Detect file encoding and load data
+st.title("Check safety status on the map ")
 with open("final_lat.csv", "rb") as f:
     encoding = chardet.detect(f.read())["encoding"]
-df = pd.read_csv("final_lat.csv", encoding=encoding)
-
-# Get unique places in West Bengal
+df = pd.read_csv("final_lat.csv", encoding=encoding
 city_list = df[df['states'] == 'West bengal']['PLACE'].unique()
 
 def create_dataframe(loca):
     """Create a crime heatmap centered on the specified location."""
-    # Retrieve latitude, longitude, cluster, and level for the specified location
+    
     lat = df[df['PLACE'] == loca]['latitude'].unique()[0]
     lng = df[df['PLACE'] == loca]['longitude'].unique()[0]
     cluster = df[df['PLACE'] == loca]['cluster'].unique()[0]
@@ -43,7 +40,7 @@ def create_dataframe(loca):
         0.8: 'orange',
         1.0: 'red'     # high intensity
     }
-    # Add heatmap to the map based on latitude, longitude, and intensity
+    # heatmap to the map based on latitude, longitude, and intensity
     HeatMap(data=coords[['latitude', 'longitude',"intse"]].values, blur=20, radius=8,gradient=gradient,blurr=1).add_to(crime_map)
     return crime_map,level
 
@@ -55,7 +52,6 @@ def user_loc(loca, map_html):
 
 def display_crime_chart(loca):
     """Display a bar chart of crime types for the selected location."""
-    # Filter data for the selected location
     location_data = df[df['PLACE'] == loca]
 
     # Group by crime type and count occurrences
@@ -73,15 +69,13 @@ def display_crime_chart(loca):
     ax.set_xlabel('Crime Type')
     ax.set_ylabel('Count')
     st.pyplot(fig)
-# Streamlit dropdown for city selection
-city_list = [" "] + list(city_list)  # Add a blank option at the beginning
+city_list = [" "] + list(city_list)
 option = st.selectbox("Select city", city_list)
 
 if option and option != " ":
     crime_map,level = create_dataframe(option)
     crime_map.save("crime_map.html")
     
-    # Read the saved map HTML and display it in Streamlit
     with open("crime_map.html", "r") as f:
         map_html = f.read()
     user_loc(option, map_html)
